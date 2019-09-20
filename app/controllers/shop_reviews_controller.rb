b7e0b358bccc6092d6ee7328a1ae47c3780d9ec1
suzken-drill <suzken@wcf-drill.com>
2019-09-20 15:18:39 +0900

    インデント修正

--- app/controllers/shop_reviews_controller.rb
+++ app/controllers/shop_reviews_controller.rb
@@ -12,25 +12,25 @@ class ShopReviewsController < ApplicationController
 
   # ショップ別口コミ一覧
   def index
-  	@shop = Shop.includes(shop_reviews: [:shop_review_evals, :shop_review_likes, :shop_review_photos]).find(params[:shop_id])
+    @shop = Shop.includes(shop_reviews: [:shop_review_evals, :shop_review_likes, :shop_review_photos]).find(params[:shop_id])
     @shop_reviews = @shop.shop_reviews.paginate(page: params[:page], per_page: ShopReview::PER_PAGE)
   end
 
   def show
     @shop = Shop.includes(shop_reviews: [:shop_review_evals, :shop_review_likes, :shop_review_photos]).find(params[:shop_id])
-  	@shop_review = ShopReview.find(params[:id])
+    @shop_review = ShopReview.find(params[:id])
   end
 
   def new
-  	@shop = Shop.find(params[:shop_id])
-  	@shop_review = ShopReview.new
-  	5.times { |i| @shop_review.shop_review_evals.build(eval_key: ShopReviewEval::EVAL_KEYS[i]) }
+    @shop = Shop.find(params[:shop_id])
+    @shop_review = ShopReview.new
+    5.times { |i| @shop_review.shop_review_evals.build(eval_key: ShopReviewEval::EVAL_KEYS[i]) }
   end
 
   def create
-  	@shop_review = ShopReview.new(shop_review_params)
-  	@shop_review.user_id = current_user.id
-  	@shop = Shop.find(@shop_review.shop_id)
+    @shop_review = ShopReview.new(shop_review_params)
+    @shop_review.user_id = current_user.id
+    @shop = Shop.find(@shop_review.shop_id)
     if @shop_review.save
       redirect_to @shop, notice: I18n.t(:shop_review_insert, scope: [:message, :shop_review])
     else
